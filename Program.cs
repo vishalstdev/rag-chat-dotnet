df91931606cc86499933fa9836b3c870fd886c68
ehsitva <vishal.b.thakur@ericsson.com>
2025-12-09 10:58:02 +0000

    Initial SK setup

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+// You'll add OpenAI key later - for now just structure
+var kernel = builder.Build();
+
+Console.WriteLine("RAG Chat App - Hello World");
+Console.WriteLine("Semantic Kernel initialized successfully!");
+
